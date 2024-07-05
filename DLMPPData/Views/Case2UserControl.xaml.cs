845159fc687619742d682e35d4289d16b68bcefc
bigsamxue <745350574@qq.com>
2024-07-05 15:58:53 +0800

    带有Case1,2互锁

--- DLMPPData/Views/Case2UserControl.xaml.cs
+++ DLMPPData/Views/Case2UserControl.xaml.cs
@@ -41,6 +41,8 @@ namespace DLMPPData.Views {
         public string Status_run2 { get; set; }
 
         public int RecordCount { get; set; }
+        private object locker = new object();
+
 
         public ConvertToDoubleClass ConvertClass { get; set; }
 
@@ -146,79 +148,83 @@ namespace DLMPPData.Views {
         }
 
         private void GetValue2() {
-            PPData2 = new ObservableCollection<Elements2>();
-            C1MinList = new List<string>();
-            C2MaxList = new List<string>();
-            DateList2 = new List<string>();
+            lock (locker) {
+                PPData2 = new ObservableCollection<Elements2>();
+                C1MinList = new List<string>();
+                C2MaxList = new List<string>();
+                DateList2 = new List<string>();
 
-            ConvertClass = new ConvertToDoubleClass();
+                ConvertClass = new ConvertToDoubleClass();
 
-            while (true) {
-                if (cancellationTokenSource2.Token.IsCancellationRequested) {
-                    break;
-                }
-                string status_temp_2 = App.DLM.RemoteCTRL(":STATus:CONDition?");
-                string status_temp_3 = Regex.Replace(status_temp_2, @"[\r\n]", "");
-                int Status_condition_Thread = -1;
-                string status_Thread = null;
-                string Status_run_Thread = null;
-                string Status_trg_Thread = null;
-                //因为需要调用主线程GUI的chbox控件的Text，因此需要App.Current.Dispatcher.Invoke
-                App.Current.Dispatcher.Invoke(new Action(() => {
-                    string RecordCount_Thread_temp = App.DLM.RemoteCTRL($":MEASURE:CHANNEL{case2chbox1.Text}:MIN:COUNT?");
-                    string RecordCount_Thread_temp_1 = null;
-
-                    if (RecordCount_Thread_temp.Contains($":MEAS:CHAN{case2chbox1.Text}:MIN:COUN")) {
-                        RecordCount_Thread_temp_1 = Regex.Replace(RecordCount_Thread_temp.Split(' ')[1], @"[\r\n]", "");
+                while (true) {
+                    if (cancellationTokenSource2.Token.IsCancellationRequested) {
+                        break;
                     }
-                    int RecordCount_Thread = -1;
-                    if (int.TryParse(status_temp_3, out Status_condition_Thread) && int.TryParse(RecordCount_Thread_temp_1, out RecordCount_Thread)) {
-                        status_Thread = Convert.ToString(Status_condition_Thread, 2).PadLeft(16, '0');
-                        Status_run_Thread = status_Thread.Substring(status_Thread.Length - 1, 1);
-                        Status_trg_Thread = status_Thread.Substring(status_Thread.Length - 3, 1);
+                    string status_temp_2 = App.DLM.RemoteCTRL(":STATus:CONDition?");
+                    string status_temp_3 = Regex.Replace(status_temp_2, @"[\r\n]", "");
+                    int Status_condition_Thread = -1;
+                    string status_Thread = null;
+                    string Status_run_Thread = null;
+                    string Status_trg_Thread = null;
+                    //因为需要调用主线程GUI的chbox控件的Text，因此需要App.Current.Dispatcher.Invoke
+                    App.Current.Dispatcher.Invoke(new Action(() => {
+                        string RecordCount_Thread_temp = App.DLM.RemoteCTRL($":MEASURE:CHANNEL{case2chbox1.Text}:MIN:COUNT?");
+                        string RecordCount_Thread_temp_1 = null;
 
-                    }
+                        if (RecordCount_Thread_temp.Contains($":MEAS:CHAN{case2chbox1.Text}:MIN:COUN")) {
+                            RecordCount_Thread_temp_1 = Regex.Replace(RecordCount_Thread_temp.Split(' ')[1], @"[\r\n]", "");
+                        }
+                        int RecordCount_Thread = -1;
+                        if (int.TryParse(status_temp_3, out Status_condition_Thread) && int.TryParse(RecordCount_Thread_temp_1, out RecordCount_Thread)) {
+                            status_Thread = Convert.ToString(Status_condition_Thread, 2).PadLeft(16, '0');
+                            Status_run_Thread = status_Thread.Substring(status_Thread.Length - 1, 1);
+                            Status_trg_Thread = status_Thread.Substring(status_Thread.Length - 3, 1);
 
-                    if (Status_run_Thread == "1" && Status_trg_Thread == "1" && RecordCount_Thread != 0 && RecordCount_Thread - RecordCount > 2) {
-                        //MessageBox.Show($"RecordCount_Thread:{RecordCount_Thread},RecordCount:{RecordCount}");
-                        RecordCount = RecordCount_Thread;
-
-                        HeadName2 headName = new HeadName2() { Case2Header1 = "Date", Case2Header2 = $"Min(C{case2chbox1.Text})", Case2Header3 = $"Max(C{case2chbox2.Text})" };
-                        DataContext = headName;
-
-                        var CH1_temp = Regex.Replace(App.DLM.RemoteCTRL(":MEASURE:CHANNEL" + case2chbox1.Text + ":MIN:VALUE?").Split(' ')[1], @"[\r\n]", "");
-                        var CH1 = ConvertClass.ConvertToDoubleDivide1k(CH1_temp);
-
-
-                        var CH2_temp = Regex.Replace(App.DLM.RemoteCTRL(":MEASURE:CHANNEL" + case2chbox2.Text + ":MAX:VALUE?").Split(' ')[1], @"[\r\n]", "");
-                        var CH2 = ConvertClass.ConvertToDouble(CH2_temp);
-
-                        var date_date_temp = App.DLM.RemoteCTRL(":SYSTem:CLOCk:DATE?");
-                        var date_date = Regex.Replace(date_date_temp.Split(' ')[1].Replace("\"", ""), @"[\r\n]", "");
-
-                        var date_time_temp = App.DLM.RemoteCTRL(":SYSTem:CLOCk:TIME?");
-                        var date_time = Regex.Replace(date_time_temp.Split(' ')[1].Replace("\"", "").Replace("\"", ""), @"[\r\n]", "");
-                        var date = date_date + "  " + date_time;
-
-                        PPData2.Add(new Elements2() { Date2 = date, C1Min = CH1, C2Max = CH2 });
-                        dgv2.ItemsSource = PPData2;
-
-                        if (dgv2.Items.Count > 0) {
-                            var border = VisualTreeHelper.GetChild(dgv2, 0) as Decorator;
-                            if (border != null) {
-                                var scroll = border.Child as ScrollViewer;
-                                if (scroll != null) {
-                                    scroll.ScrollToEnd();//最后一行
-                                }
-                            }
                         }
 
-                        C1MinList.Add(CH1);
-                        C2MaxList.Add(CH2);
-                        DateList2.Add(date);
-                    }
-                }));
+                        if (Status_run_Thread == "1" && Status_trg_Thread == "1" && RecordCount_Thread != 0 && RecordCount_Thread - RecordCount > 3) {
+                            //MessageBox.Show($"RecordCount_Thread:{RecordCount_Thread},RecordCount:{RecordCount}");
+                            RecordCount = RecordCount_Thread;
+
+                            HeadName2 headName = new HeadName2() { Case2Header1 = "Date", Case2Header2 = $"Min(C{case2chbox1.Text})", Case2Header3 = $"Max(C{case2chbox2.Text})" };
+                            DataContext = headName;
+
+                            var CH1_temp = Regex.Replace(App.DLM.RemoteCTRL(":MEASURE:CHANNEL" + case2chbox1.Text + ":MIN:VALUE?").Split(' ')[1], @"[\r\n]", "");
+                            var CH1 = ConvertClass.ConvertToDoubleDivide1k(CH1_temp);
+
+
+                            var CH2_temp = Regex.Replace(App.DLM.RemoteCTRL(":MEASURE:CHANNEL" + case2chbox2.Text + ":MAX:VALUE?").Split(' ')[1], @"[\r\n]", "");
+                            var CH2 = ConvertClass.ConvertToDouble(CH2_temp);
+
+                            var date_date_temp = App.DLM.RemoteCTRL(":SYSTem:CLOCk:DATE?");
+                            var date_date = Regex.Replace(date_date_temp.Split(' ')[1].Replace("\"", ""), @"[\r\n]", "");
+
+                            var date_time_temp = App.DLM.RemoteCTRL(":SYSTem:CLOCk:TIME?");
+                            var date_time = Regex.Replace(date_time_temp.Split(' ')[1].Replace("\"", "").Replace("\"", ""), @"[\r\n]", "");
+                            var date = date_date + "  " + date_time;
+
+                            PPData2.Add(new Elements2() { Date2 = date, C1Min = CH1, C2Max = CH2 });
+                            dgv2.ItemsSource = PPData2;
+
+                            if (dgv2.Items.Count > 0) {
+                                var border = VisualTreeHelper.GetChild(dgv2, 0) as Decorator;
+                                if (border != null) {
+                                    var scroll = border.Child as ScrollViewer;
+                                    if (scroll != null) {
+                                        scroll.ScrollToEnd();//最后一行
+                                    }
+                                }
+                            }
+
+                            C1MinList.Add(CH1);
+                            C2MaxList.Add(CH2);
+                            DateList2.Add(date);
+                        }
+                    }));
+                }
             }
+
+
         }
     }
 
