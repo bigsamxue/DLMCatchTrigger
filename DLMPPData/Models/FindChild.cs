7b9b2c94cf4f6d8af4aff9b4f793cee5aa1e7f35
bigsamxue <745350574@qq.com>
2024-07-05 16:39:20 +0800

    Case1,2互锁

---
+++ DLMPPData/Models/FindChild.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+
+namespace DLMPPData.Models {
+    public class FindChild {
+        /// <summary>
+        /// 找Control
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="parent"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public T FindVisualChild<T>(DependencyObject parent, string name) where T : FrameworkElement {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++) {
+                DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+                if (child is T element && element.Name == name) {
+                    return element;
+                }
+                T childOfChild = FindVisualChild<T>(child, name);
+                if (childOfChild != null) {
+                    return childOfChild;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ DLMPPData/Models/FindWindow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DLMPPData.Models {
+    public class FindWindow {
+        /// <summary>
+        /// 找Window
+        /// </summary>
+        /// <param name="userControl"></param>
+        /// <returns></returns>
+        public Window GetParentWindow(UserControl userControl) {
+            DependencyObject parent = VisualTreeHelper.GetParent(userControl);
+
+            while (parent != null && !(parent is Window)) {
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            return parent as Window;
+        }
+    }
+}
